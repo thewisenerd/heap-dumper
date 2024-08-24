c2d06346cd9a5e53990f9178a43fa1dcadf5fa7d
thewisenerd <thewisenerd@protonmail.com>
2024-08-24 14:58:24 +0530

    fix string decoding; support large buffer direct copy

--- src/main/kotlin/io/github/thewisenerd/experiments/HeapDumpTest.kt
+++ src/main/kotlin/io/github/thewisenerd/experiments/HeapDumpTest.kt
@@ -1,12 +1,14 @@
 package io.github.thewisenerd.experiments
 
-import java.io.BufferedOutputStream
+import org.netbeans.lib.profiler.heap.*
 import java.io.File
 import java.io.OutputStream
-import org.netbeans.lib.profiler.heap.HeapFactory
-import org.netbeans.lib.profiler.heap.Instance
-import org.netbeans.lib.profiler.heap.ObjectArrayInstance
-import org.netbeans.lib.profiler.heap.PrimitiveArrayInstance
+import java.io.RandomAccessFile
+import java.nio.ByteBuffer
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
 
 inline fun <reified T> Instance.cast(): T {
     if (this is T) return this
@@ -15,24 +17,55 @@ inline fun <reified T> Instance.cast(): T {
 
 object Constants {
     const val EMPTY_STRING = ""
+    const val LATIN1: Byte = 0
+    const val UTF16: Byte = 1
+    const val ONE_MB = 1024 * 1024
+    const val STR_LIMIT = 4 * ONE_MB
+
+    const val ID_SIZE = 8
+
+    val fileOffsetCls = Class.forName("org.netbeans.lib.profiler.heap.HprofObject")
+    val fileOffsetAccessor = fileOffsetCls.getDeclaredField("fileOffset")
+
+    init {
+        fileOffsetAccessor.isAccessible = true
+    }
 }
 
-fun Instance.getString(): String? {
+
+fun Instance.getString(raf: RandomAccessFile): String? {
     val clazz = this.javaClass
     if (clazz.name != "java.lang.String") return null
+
+    val coder = this.getValueOfField("coder") as Byte
 
     val array = this.getValueOfField("value") as PrimitiveArrayInstance
     if (array.values.isEmpty()) return Constants.EMPTY_STRING
 
-    // let me read package private classes ,_,
+    val strLength = array.length
+    val needsTriage = array.length > Constants.STR_LIMIT
 
-    val buffer = StringBuffer(array.length)
-    array.values.filterNotNull().forEach { buffer.append(it as String) }
+    val buffer = ByteArray(strLength)
+    val fileOffset = Constants.fileOffsetAccessor.get(array) as Long
+    val arrayStartOffset = fileOffset + 1 + Constants.ID_SIZE + 4 + 4 + 1
 
-    return buffer.toString()
+    raf.seek(arrayStartOffset)
+    raf.read(buffer, 0, strLength)
+
+    val decoded = when (coder) {
+        Constants.LATIN1 -> String(buffer, Charsets.UTF_8)
+        Constants.UTF16 -> String(buffer, Charsets.UTF_16LE)
+        else -> throw IllegalArgumentException("unknown coder value=${coder.toInt()}")
+    }
+
+    if (needsTriage) {
+        println("line needs triage: ${decoded.length}")
+    }
+
+    return decoded
 }
 
-fun dump(array: ObjectArrayInstance, os: OutputStream) {
+fun dump(array: ObjectArrayInstance, raf: RandomAccessFile, os: OutputStream) {
     val len0 = array.values.size
     array.values.asSequence().mapNotNull { it as? Instance }.forEachIndexed { idx0, instance ->
         println("processing object $idx0 / $len0")
@@ -40,18 +73,28 @@ fun dump(array: ObjectArrayInstance, os: OutputStream) {
         when (clazz.name) {
             "java.util.Collections\$SingletonList" -> {
                 val element = instance.getValueOfField("element")?.let { it as Instance }
-                element?.getString()?.let {
+                element?.getString(raf)?.let {
                     os.write("$it\n".toByteArray())
                 }
             }
+
             "java.util.ArrayList" -> {
                 val elementData = instance.getValueOfField("elementData") as ObjectArrayInstance
                 val len1 = elementData.values.size
                 elementData.values.asSequence().mapNotNull { it as? Instance }.forEachIndexed { idx1, element ->
                     println("processing object $idx0 / $len0; $idx1 / $len1")
-                    element.getString()?.let {
+                    element.getString(raf)?.let {
                         os.write("$it\n".toByteArray())
                     }
+                }
+            }
+
+            "java.lang.String" -> {
+                instance.getString(raf)?.let {
+                    it.chunked(64 * Constants.ONE_MB).forEach { chunk ->
+                        os.write(chunk.toByteArray())
+                    }
+                    os.write("\n".toByteArray())
                 }
             }
         }
@@ -60,12 +103,17 @@ fun dump(array: ObjectArrayInstance, os: OutputStream) {
 }
 
 fun main() {
-    val heapFile = File("/Users/thewisenerd/works/data/2022-01-13/gc220113_01.hprof")
+    val heapFilePath = "/Users/thewisenerd/works/data/2024-08-23/java_pid1.hprof"
+    val heapFile = File(heapFilePath)
     val heap = HeapFactory.createHeap(heapFile)
 
-    val outputFile = File("/Users/thewisenerd/works/data/2022-01-13/logs_220113_01_${System.currentTimeMillis()}.json").outputStream().buffered()
+    val raf = RandomAccessFile(heapFile, "r")
+
+    val outputFile =
+        File("/Users/thewisenerd/works/data/2024-08-23/logs_240823_01_${System.currentTimeMillis()}.json").outputStream()
+            .buffered()
     outputFile.use { os ->
-        dump(heap.getInstanceByID(0x6422ac568).cast(), os)
+        dump(heap.getInstanceByID(0x64a2a54d0).cast(), raf, os)
     }
 
     println("debug break")
